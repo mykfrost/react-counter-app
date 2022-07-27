a1f1ac4f355a4593215177e4a6b2d60fc752c37e
Michael Okello <okellofrost@gmail.com>
2022-07-27 10:21:33 +0300

    styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,17 +7,14 @@ class Counter extends Component {
 
          } ;
 
-         styles = {
-            fontSize : 10 ,
-            fontWeight: 'bold'
-         };
+       
 
     render() { 
         return (
         
             <React.Fragment>
            
-            <span style={this.styles} className='badge badge-primary m-2'>{this.formatCount()}</span>
+            <span style={{fontSize : 30}} className='badge badge-primary m-2'>{this.formatCount()}</span>
             <button className='btn btn-secondary btn-sm'>Increment</button>
             </React.Fragment>
             );
