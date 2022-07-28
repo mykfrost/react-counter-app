64273f268bf7069c8d583b897e0617afa3782e88
Michael Okello <okellofrost@gmail.com>
2022-07-28 11:29:50 +0300

    adding ids

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,25 +3,24 @@ import React, { Component } from 'react';
 
 
 class Counter extends Component {
-    state = { 
+         state = { 
 
-        count : 0 ,
-        tags:['tag1', 'tag2' , 'tag3']
+        value : this.props.value 
 
          } ;  
 
-             handleIncrement = product => {
-                //console.log(product);
-           this.setState({count : this.state.count + 1});
+        handleIncrement = () => {
+            this.setState({value : this.state.value + 1});
         };
 
     render() { 
-       
+      // console.log('props',this.props);
         return (         
             <div>
+            <h4>{this.props.id}</h4>
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button
-             onClick= { (product)  => this.handleIncrement(product)}
+             onClick= { this.handleIncrement}
              className='btn btn-secondary btn-sm'
              >
                 Increment
@@ -40,13 +39,13 @@ class Counter extends Component {
 
      getBadgeClasses() {
         let classes = 'badge m-2 badge-';
-        classes += this.state.count === 0 ? 'warning' : 'primary';
+        classes += this.state.value === 0 ? 'warning' : 'primary';
         return classes;
     }
     formatCount(){
         //using object destructuring
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
+        const {value} = this.state;
+        return value === 0 ? 'Zero' : value;
     }
 }
 
