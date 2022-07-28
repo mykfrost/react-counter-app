24bf257e6efdf4c730b9d5ded95cac7cd8b779dd
Michael Okello <okellofrost@gmail.com>
2022-07-28 18:01:29 +0300

    Method destrructuring

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 //stateless functional components
-const NavBar = (props) =>{
+const NavBar = ({totalCounters}) =>{
 
   return (
   <nav className="navbar bg-light">
   <div className="container-fluid">
-  <a className="navbar-brand" href="#">NavBar<span className="badge badge badge-pill badge-secondary m-2">{props.totalCounters}</span></a>
+  <a className="navbar-brand" href="#">NavBar
+  <span className="badge badge badge-pill badge-secondary m-2">
+    {totalCounters}</span></a>
   </div>
   </nav>
   
